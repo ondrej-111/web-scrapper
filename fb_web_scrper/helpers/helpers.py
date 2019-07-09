2f708ca429d719da74b9bba5c4ea27c79697f3aa
Ondrej Zoričák <1ondrej.zoricak@gmail.com>
2019-07-09 19:25:31 +0200

    edit parsing results

--- fb_web_scrper/helpers/helpers.py
+++ fb_web_scrper/helpers/helpers.py
@@ -1,8 +1,10 @@
-from lxml.html import fromstring
 import requests
 from flask import abort, make_response, jsonify
 from .Config import Config
-from urllib.parse import unquote
+from bs4 import BeautifulSoup
+
+USER_AGENT = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
 
 def get_bool(bool_value):
@@ -16,20 +18,20 @@ def google_search(search_input, proxy):
 
 
 def google_search_crawlera(search_input):
-    return requests.get('https://www.google.com/search', params={'q': search_input}, proxies={
-        'http': Config.get('DEFAULT', 'crawlera_url')
+    return requests.get('https://www.google.com/search', headers=USER_AGENT, params={'q': search_input}, proxies={
+        'http': 'http://{}'.format(Config.get('DEFAULT', 'crawlera_uri'))
     })
 
 
 def get_first_result(html):
-    parser = fromstring(html)
+    soup = BeautifulSoup(html, 'html.parser')
 
     try:
-        aes = parser.xpath('//body/div/div/div/div/a[1]')
-        if len(aes) > 0:
-            sub_a = aes[1].attrib['href'][len('/url?q='):]
-            return unquote(sub_a[:sub_a.find('&sa=U')])
-        else:
-            abort(make_response(jsonify(message='No results founds.'), 409))
+        link = None
+        result_block = soup.find_all('div', attrs={'class': 'g'})
+        for result in result_block:
+            link = result.find('a', href=True)
+            break
+        return link['href']
     except IndexError:
         abort(make_response(jsonify(message='No results founds.'), 409))
