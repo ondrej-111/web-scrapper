fa1949e5cf3440c6bd054774da0aba1224bcabdd
Ondrej Zoričák <1ondrej.zoricak@gmail.com>
2019-10-06 20:00:45 +0200

    move structure for deploying

--- app/main/google_search/models/dto.py
+++
@@ -1,8 +0,0 @@
-from flask_restplus import Namespace, fields
-
-
-class SearchResultDto:
-    ns = Namespace('search', description='Search for first result from google search engine')
-    user_auth = ns.model('query', {
-        'url': fields.String(required=True, description='result url'),
-    })
